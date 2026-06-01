5497c6443980bdf1b12f55686970399bb9928280
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-06-01 23:27:51 +0545

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.js
@@ -0,0 +1,59 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encoded = "";
+        for (let i = 0; i < strs.length; i++) {
+            const strLen = strs[i].length;
+            encoded = encoded + strLen + "#" + strs[i];
+
+        }
+        return encoded;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let prevStrEndIndex = 0;
+        let strs=[];
+
+        for (let k = 0; k < str.length; k++) {
+            if (prevStrEndIndex >= str.length) {
+                break;
+            }
+
+            let strLength = ""
+            let strLengthIndicatorEndIndex = 0;
+
+            for (let i = prevStrEndIndex; i < str.length; i++) {
+                if (str[i] == "#") {
+                    strLengthIndicatorEndIndex = i
+                    strLength = Number(strLength)
+                    break;
+                }
+                strLength = strLength + str[i]
+            }
+
+
+
+
+            const start = strLengthIndicatorEndIndex + 1;
+            const end = Number(strLengthIndicatorEndIndex + 1 + strLength)
+            console.log(prevStrEndIndex, strLengthIndicatorEndIndex, strLength, start, end)
+            const text = str.slice(start, end)
+            //console.log(text)
+            strs.push(text)
+            prevStrEndIndex = end;
+            console.log(prevStrEndIndex)
+
+        }
+
+        return strs
+
+
+    }
+}
