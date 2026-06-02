a32aa6d2ee8dafbbea4b1bc0158786b974aa5e58
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-06-02 21:19:27 +0545

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const prefixArr = new Array(n);
+        const postfixArr = new Array(n);
+        const productList = new Array(n);
+
+        prefixArr[0] = 1;
+        postfixArr[n - 1] = 1;
+
+        for (let i = 1; i < n; i++) {
+            prefixArr[i] = prefixArr[i - 1] * nums[i - 1]
+        }
+
+        productList[n-1] = prefixArr[n-1] * 1;
+
+        for (let i = n - 2; i >= 0; i--) {
+            postfixArr[i] = postfixArr[i + 1] * nums[i + 1]
+            productList[i] = prefixArr[i] * postfixArr[i]
+        }
+
+        
+
+       // console.log(prefixArr, postfixArr)
+        return productList;
+    }
+}
