b3e17922b79d4ef39734330c5026315b76ec232a
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-06-08 18:21:01 +0545

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let i = 0;
+        let j = numbers.length - 1;
+        let indices = [];
+
+        while (i < j) {
+            const leftNumber = numbers[i];
+            const rightNumber = numbers[j]
+            if (leftNumber + rightNumber === target) {
+                indices.push(i + 1);
+                indices.push(j + 1);
+                return indices;
+            }
+            if (leftNumber + rightNumber > target) {
+                j--;
+            }
+            if (leftNumber + rightNumber < target) {
+                i++;
+            }
+        }
+
+        return indices;
+    }
+}
