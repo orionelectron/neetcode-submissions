12a222ee305fbba812ad06a6750c2660fa0bda63
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-06-12 11:12:24 +0545

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let result = 0;
+        let maxLeftArr = new Array(height.length).fill(0);
+        let maxRightArr = new Array(height.length).fill(0);
+
+        maxLeftArr[0] = height[0];
+        maxRightArr[height.length - 1] = height[height.length - 1];
+
+        console.log(maxLeftArr, maxRightArr)
+
+
+        for (let i = 1; i < height.length; i++) {
+            maxLeftArr[i] = Math.max(height[i], maxLeftArr[i - 1])
+        }
+
+        for (let j = height.length - 2; j >= 0; j--) {
+            maxRightArr[j] = Math.max(height[j], maxRightArr[j + 1])
+        }
+
+        for (let k = 0; k < height.length; k++) {
+            const currentValue = Math.min(maxLeftArr[k], maxRightArr[k]) - height[k];
+            result = currentValue >= 0 ? result + currentValue : result;
+        }
+
+
+        return result;
+
+    }
+}
