ac78e37858e2d9ce134c93514fdc549fcfba60b6
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-06-18 13:04:48 +0545

    Add: largest-rectangle-in-histogram - submission-0

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.js
@@ -0,0 +1,59 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+
+    peek(stack) {
+        return stack[stack.length - 1] !== undefined ? stack[stack.length - 1] : {
+            position: -1,
+            height: -1
+        };
+    }
+
+    largestRectangleArea(heights) {
+        let stack = [];
+        let maxArea = 0;
+
+
+
+        for (let i = 0; i < heights.length; i++) {
+            const currentHeight = heights[i];
+            maxArea = Math.max(maxArea, currentHeight)
+
+            let left = i;
+            let right = i;
+
+            //process left side
+            for (let j = i - 1; j >= 0; j--) {
+                const height = heights[j];
+
+                if (currentHeight > height) {
+                    break;
+                }
+
+                left = j;
+
+            }
+
+            //process right side
+            for (let j = i + 1; j < heights.length; j++) {
+
+                const height = heights[j];
+                if (currentHeight > height) {
+                    break;
+                }
+
+                right = j;
+            }
+            //console.log(left, right)
+
+            maxArea = Math.max(((right - left) + 1) * currentHeight, maxArea)
+
+        }
+
+
+
+        return maxArea;
+    }
+}
