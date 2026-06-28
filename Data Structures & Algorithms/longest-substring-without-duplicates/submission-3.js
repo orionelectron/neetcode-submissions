c1644372d2692992203ef502e3cf3a65602369ee
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-06-28 11:34:22 +0545

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let left = 0;
+        let right = 0;
+        let max = 0;
+        const set = new Set();
+
+        while (right < s.length) {
+            while (set.has(s[right])) {
+                set.delete(s[left]);
+                left++;
+            }
+
+            set.add(s[right]);
+            max = Math.max(max, set.size);
+            right++;
+        }
+
+        return max;
+    }
+}
