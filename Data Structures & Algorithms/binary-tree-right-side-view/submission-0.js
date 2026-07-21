8a2ad352fa28fc300f22cb3509e01332a21f7143
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-07-21 10:52:11 +0545

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        let list = [];
+
+        let queue = [];
+
+        queue.push(root);
+
+        while (queue.length > 0) {
+            let temp = [];
+            const currentQlen = queue.length;
+            for (let i = 0; i < currentQlen; i++) {
+                const node = queue.shift();
+                if (node) {
+                    queue.push(node.left);
+                    queue.push(node.right);
+                    temp.push(node.val);
+                }
+            }
+            if (temp.length > 0) {
+                list.push(temp[temp.length - 1]);
+            }
+        }
+        console.log(list);
+        return list;
+    }
+}
