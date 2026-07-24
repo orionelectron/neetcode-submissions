ffb3162374d350ae3863f614ff106fa310ab54ad
Pawan Kandel <61446548+orionelectron@users.noreply.github.com>
2026-07-24 10:41:33 +0545

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {number[]} preorder
+     * @param {number[]} inorder
+     * @return {TreeNode}
+     */
+
+    buildTree(preorder, inorder) {
+        let nextPreorderIndex = 0;
+        let inorderMap = {};
+        for (let i = 0; i < inorder.length; i++) {
+            inorderMap[inorder[i]] = i;
+        }
+        const buildRecur = (preorder, inorder, start, end) => {
+            if (start > end) {
+                return null;
+            }
+
+            const rootVal = preorder[nextPreorderIndex++];
+            let leftIndices = { start: -1, end: -1 };
+            let rightIndices = { start: -1, end: -1 };
+
+            let mid = inorderMap[rootVal];
+
+            const leftSubtree = buildRecur(preorder, inorder, start, mid - 1);
+            const rightSubtree = buildRecur(preorder, inorder, mid + 1, end);
+            const rootNode = new TreeNode(rootVal, leftSubtree, rightSubtree);
+            return rootNode;
+        };
+        return buildRecur(preorder, inorder, 0, inorder.length - 1);
+    }
+}
